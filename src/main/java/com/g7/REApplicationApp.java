3013896905ae6edbba7b3079a00c4b962c9a4249
jingbinwang <79555784+bluebluefrog@users.noreply.github.com>
2022-08-09 16:29:11 +1000

    first commit

---
+++ src/main/java/com/g7/REApplicationApp.java
@@ -0,0 +1,14 @@
+package com.g7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class REApplicationApp {
+
+    public static void main(String[] args) {
+        ApplicationContext app = SpringApplication.run(REApplicationApp.class, args);
+
+    }
+}
